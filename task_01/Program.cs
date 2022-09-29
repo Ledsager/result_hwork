92e174814d93c3a5cb4f3d0b104515750109b928
«Rustam-Ledsager» <r.yakupov81@yandex.ru>
2022-09-29 23:48:42 +0300

    added void Print and Input massiv

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+// формирует новый массив из строк, длина которых меньше, либо равна 
+// 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется 
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Метод вывода массива на экран
+void PrintArray(string[] position)
+{
+    foreach (var element in position)
+        Console.Write($"'{element}', ");
+    Console.WriteLine();
+}
+
+// Метод ввода массива с клавиатуры
+void InputMas(string[] position)
+{
+    for (int i = 0; i < position.Length; i++)
+    {
+        Console.WriteLine("Введите {0}-й элемент (строку): ", i + 1);
+        position[i] = Console.ReadLine();
+    }
+}
+
+
+
+Console.Write("Введите длинну массива:");
+int LengthArray = int.Parse(Console.ReadLine());
+string [] array = new string[LengthArray];
+InputMas(array);
+
+PrintArray(array);
