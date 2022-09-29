def0c06e2e6c112a4147c81a7e7758a545b7ddf5
«Rustam-Ledsager» <r.yakupov81@yandex.ru>
2022-09-30 01:08:16 +0300

    added void ArrayShort and ElementOfShort

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -5,28 +5,61 @@
 // пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 // Метод вывода массива на экран
-void PrintArray(string[] position)
+void PrintArray(string[] array)
 {
-    foreach (var element in position)
-        Console.Write($"'{element}', ");
-    Console.WriteLine();
-}
-
-// Метод ввода массива с клавиатуры
-void InputMas(string[] position)
-{
-    for (int i = 0; i < position.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.WriteLine("Введите {0}-й элемент (строку): ", i + 1);
-        position[i] = Console.ReadLine();
+        Console.WriteLine("{0}-й элемент (строка): {1}", i + 1, array[i]);
+        Console.WriteLine();
     }
 }
 
+// Метод ввода массива с клавиатуры
+void InputMas(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите {0}-й элемент (строку): ", i + 1);
+        array[i] = Console.ReadLine();
+    }
+}
 
+// Подсчет количества элементов длина которых <=3 для формирования длины массива в который эти элементы запишутся
+int ElementOfShort(string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            j++;
+    }
+    return j;
+}
+
+// Заполнение нового массива элементами 1го массива , длина которых <=3
+string[] ArrayShort(string[] array)
+{
+    string[] arrayshort = new string[ElementOfShort(array)];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+
+        if (array[i].Length <= 3)
+        {
+            arrayshort[j] = array[i];
+            j++;
+        }
+return arrayshort;
+}
 
 Console.Write("Введите длинну массива:");
-int LengthArray = int.Parse(Console.ReadLine());
-string [] array = new string[LengthArray];
+int lengtharray = int.Parse(Console.ReadLine());
+string[] array = new string[lengtharray];
 InputMas(array);
+/*
+ string[] arraymodification = array;
+arraymodification = arraymodification.Where(e => e.Length <= 3).ToArray();
+Console.WriteLine(String.Join(",", arraymodification));
+*/
+Console.WriteLine("Элементы нового массива:");
+PrintArray(ArrayShort(array));
 
-PrintArray(array);
